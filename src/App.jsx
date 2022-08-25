49e67da97b077636e9cbd74510ce58a22dff6588
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-25 15:16:28 +0300

    feat: add material UI styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import BasicSearch from './components/BasicSearch';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BasicSearch />
+    </div>
+  );
 }
 
 export default App;
