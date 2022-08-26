e69c4580e7fa127761799e0f32cdbb2f3f3c0238
Rodgers Chayuga <rodgers.chayuga@baanx.com>
2022-08-26 16:49:00 +0300

    fix: fix api setup

--- api/app.js
+++ api/app.js
@@ -1,11 +1,16 @@
-const express = require('express');
+import express from 'express';
+import cors from 'cors';
+import { Users } from './users.js';
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello world! from Nairobi');
+  res.json(Users);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`API is working on port: ${port}`);
 });
